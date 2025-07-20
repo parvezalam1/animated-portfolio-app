217970045ee337f449fc078cc803aab2c23e0ff2
saif <saif@gmail.com>
2025-07-20 15:18:32 +0530

    vercel.json file has been changed

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output:"export",
+    output:"export",
     images: {
-        // loader:"custom",
-        // loaderFile:"./loader.js",
+        loader:"custom",
+        loaderFile:"./loader.js",
         remotePatterns: [
             {
 
