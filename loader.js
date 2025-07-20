f611e268e1bb6ad504a6e2fc238d874bcf3add93
saif <saif@gmail.com>
2025-07-20 15:20:51 +0530

    vercel.json file has been changed

--- loader.js
+++ loader.js
@@ -3,5 +3,5 @@
 export default function myImageLoader({ src, width, quality }) {
     if(src.startsWith("https://img-c.udemycdn.com") || src.startsWith("https://encrypted-tbn0.gstatic.com") || 
     src.startsWith("https://img1.daumcdn.net") || src.startsWith("https://images.prismic.io")) return src
-  return `http://localhost:3000/${src}?w=${width}&q=${quality || 75}`
+  return `https://animated-portfolio-app.vercel.app/${src}?w=${width}&q=${quality || 75}`
 }
