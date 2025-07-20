8ad8f5dcdda3679c8da05d98012a03ca6e2f8df8
saif <saif@gmail.com>
2025-07-20 15:15:39 +0530

    vercel.json file has been changed

--- loader.js
+++ loader.js
@@ -1,13 +1,7 @@
 'use client'
  
 export default function myImageLoader({ src, width, quality }) {
-    if(src.startsWith("https://img-c.udemycdn.com")) return src
-  return `https://animated-portfolio-app.vercel.app/${src}?w=${width}&q=${quality || 75}`
+    if(src.startsWith("https://img-c.udemycdn.com") || src.startsWith("https://encrypted-tbn0.gstatic.com") || 
+    src.startsWith("https://img1.daumcdn.net") || src.startsWith("https://images.prismic.io")) return src
+  return `http://localhost:3000/${src}?w=${width}&q=${quality || 75}`
 }
-
-
-// export default function myImageLoader({ src, width, quality }) {
-//     if(src.startsWith("https://img-c.udemycdn.com") || src.startsWith("https://encrypted-tbn0.gstatic.com") || 
-//     src.startsWith("https://img1.daumcdn.net") || src.startsWith("https://images.prismic.io")) return src
-//   return `https://animated-portfolio-app.vercel.app/${src}?w=${width}&q=${quality || 75}`
-// }
